f9b08ea5331d22a8168d90c5078a87b4cbadd3e7
Rebecca <rebecca.solomon446@gmail.com>
2022-12-08 17:00:17 +0000

    created passengers and flights table and joined them to show many to many relationship

--- src/main/java/com/example/airline_api/models/Flight.java
+++ src/main/java/com/example/airline_api/models/Flight.java
@@ -1,15 +1,32 @@
 package com.example.airline_api.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-
+@Entity@Table(name = "estates")
 public class Flight {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private long id;
+
+    @Column( name = "destination")
     private String destination;
+
+    @Column( name = "capacity")
     private int capacity;
+
+    @Column( name = "departureDate")
     private String departureDate;
+
+    @Column(name = "departureTime")
     private String departureTime;
+
+    @JsonIgnoreProperties({"flights"})
+    @ManyToMany(mappedBy = "flights")
     private List<Passenger> passengers;
 
     public Flight(String destination, int capacity, String departureDate, String departureTime) {
--- src/main/java/com/example/airline_api/models/Passenger.java
+++ src/main/java/com/example/airline_api/models/Passenger.java
@@ -1,13 +1,32 @@
 package com.example.airline_api.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-
+@Entity
+@Table(name = "passengers")
 public class Passenger {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "email")
     private String email;
+
+    @ManyToMany
+    @JoinTable(
+            name = "passengers_flights",
+            joinColumns = @JoinColumn(name = "passenger_id"),
+            inverseJoinColumns = @JoinColumn(name = "chocolate_id")
+    )
+    @JsonIgnoreProperties({"passengers"})
     private List<Flight> flights;
 
     public Passenger(String name, String email) {
